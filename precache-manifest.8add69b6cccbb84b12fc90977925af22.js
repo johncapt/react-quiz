fa5b72c3db8ae6339d3dced160e2ddca679fe9b3
jfccapt <jfc.capt@gmail.com>
2020-08-26 20:19:08 +0800

    Updates

---
+++ precache-manifest.8add69b6cccbb84b12fc90977925af22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077883f24f019e15ca19a4dfd9d6b9a7",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "6c3a83c74262e4053e25",
+    "url": "/react-quiz/static/css/main.049788eb.chunk.css"
+  },
+  {
+    "revision": "18d5ee13fac898346dff",
+    "url": "/react-quiz/static/js/2.e3a131b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-quiz/static/js/2.e3a131b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3a83c74262e4053e25",
+    "url": "/react-quiz/static/js/main.1980130d.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "1a624abdbcc916424ac14a50f08a9df6",
+    "url": "/react-quiz/static/media/Australia.1a624abd.png"
+  }
+]);
